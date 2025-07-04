4d4f79e97c2a09c7ff2057f2ff94e9977bae38d5
Achraf-laff <khemirachraf95@gmail.com>
2025-07-04 11:25:24 +0100

    backend

---
+++ routes/course.route.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  buyCourses,
+  buyCourseWithoutPayment,
+  courseDetails,
+  createCourse,
+  deleteCourse,
+  getCourses,
+  updateCourse,
+} from "../controllers/course.controller.js";
+import userMiddleware from "../middlewares/user.mid.js";
+import adminMiddleware from "../middlewares/admin.mid.js";
+
+const router = express.Router();
+
+router.post("/create", adminMiddleware, createCourse);
+router.put("/update/:courseId", adminMiddleware, updateCourse);
+router.delete("/delete/:courseId", adminMiddleware, deleteCourse);
+
+router.get("/courses", getCourses);
+router.get("/:courseId", courseDetails);
+
+// router.post("/buy/:courseId", userMiddleware, buyCourses);
+
+// Route to purchase a course without payment
+router.post("/buy/:courseId", userMiddleware, buyCourseWithoutPayment);
+
+export default router;
